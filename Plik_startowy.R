a7e5fba827a829b0fb73c24acda872d06276f646
lucas9999 <memy@wp.pl>
2019-08-18 19:37:40 +0200

    ,

---
+++ Plik_startowy.R
@@ -0,0 +1,11 @@
+bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE)
+
+# shell.exec(paste0(getwd(), '/_book/index.html' ) )
+# 
+# ```{r, eval=TRUE, echo=FALSE, fig.cap='Instalation of package from local files', out.width='500px'}
+# knitr::include_graphics('figures/git_repo_Cloning.png')
+# ```
+
+
+# Inline-style: 
+#   ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
